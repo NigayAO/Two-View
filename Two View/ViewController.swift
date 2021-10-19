5babb8e0f1f985cf225078fed378766217dc0738
Alik <Nigay_AO@bk.ru>
2021-10-19 11:43:10 +0300

    Initial Commit

---
+++ Two View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two View
+//
+//  Created by Alik Nigay on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
